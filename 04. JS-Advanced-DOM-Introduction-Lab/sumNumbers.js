fa03c2d9b96acd8299a9897216bf00583421125e
l-damyanov <l_l_damyanov@abv.bg>
2021-09-24 13:57:49 +0300

    DOM introduction lab

---
+++ 04. JS-Advanced-DOM-Introduction-Lab/sumNumbers.js
@@ -0,0 +1,8 @@
+function calc() {
+    const num1 = Number(document.getElementById('num1').value);
+    const num2 = Number(document.getElementById('num2').value);
+
+    const sum = num1 + num2;
+
+    document.getElementById('sum').value = sum;
+}
